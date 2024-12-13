0a4f0cad73613fa2f8e9c1fc106f34ea2a4003cc
Henri Bresteau <henri.bresteau@gmail.com>
2024-12-13 17:53:55 +0100

    [ADD] Magic ui - Marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,jsx}"],
     theme: {
         extend: {
@@ -11,11 +12,71 @@ export default {
                 Inter600: "Inter Semi Bold, cursive",
                 Inter700: "Inter Bold, cursive",
             },
-            backgroundImage: {
-                "hero-pattern": "url('/src/assets/hero-bg.svg')",
-                "hero-pattern-v": "url('/src/assets/hero-bg-v.svg')",
+            // backgroundImage: {
+            // 	// 'hero-pattern': 'url('/src/assets/hero-bg.svg')')',
+            // 	// 'hero-pattern-v': 'url("/src/assets/hero-bg-v.svg")',
+            // },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            colors: {
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                chart: {
+                    1: "hsl(var(--chart-1))",
+                    2: "hsl(var(--chart-2))",
+                    3: "hsl(var(--chart-3))",
+                    4: "hsl(var(--chart-4))",
+                    5: "hsl(var(--chart-5))",
+                },
+            },
+            keyframes: {
+                marquee: {
+                    from: {
+                        transform: "translateX(0)",
+                    },
+                    to: {
+                        transform: "translateX(calc(-100% - var(--gap)))",
+                    },
+                },
+            },
+            animation: {
+                marquee: "marquee var(--duration) infinite linear",
             },
         },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
